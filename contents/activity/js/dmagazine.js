5d979765c991f80e4b70a67c91ee8c650fb324c2
jaeyoung-design <sjy980130@naver.com>
2024-12-24 18:01:01 +0900

    dmagazine 수정

--- contents/activity/js/dmagazine.js
+++ contents/activity/js/dmagazine.js
@@ -2,6 +2,17 @@
 
 
 //alert('test!')
+
+////////////////// 전역함수 설정 //////////////////
+//활성화된 뉴스 컨텐츠 제외 모두 숨기기
+/* var onNews = $('.tab-menu .on').find('a').attr('href');
+//console.log('기본 활성화된 뉴스: ' + onNews);
+
+//활성화 된 뉴스 컨텐츠 li 갯수
+var onNewsCount = $(onNews).find('li').length;
+//console.log('li의 갯수: ' + onNewsCount); */
+
+
 $(document).ready(function () {
 
     ////////////////// load more 구현 //////////////////
@@ -10,35 +21,20 @@ $(document).ready(function () {
     var onNews = $('.tab-menu .on').find('a').attr('href');
     //console.log('기본 활성화된 뉴스: ' + onNews);
 
+    //활성화 된 뉴스 컨텐츠 li 갯수
     var onNewsCount = $(onNews).find('li').length;
-    console.log(onNewsCount);
+    //console.log('li의 갯수: ' + onNewsCount);
 
+
+    //활성화 되지 않은 컨텐츠들 숨기기
     $(onNews).siblings().hide();
     //활성화된 뉴스 컨텐츠 중에서 보여질 구간 설정
     $(onNews).find('li').slice(6, onNewsCount).hide();
 
-    $('.load-more').on('click', function () {
 
-        var hNews = $(onNews).find('li:hidden').length;
-        console.log('숨겨진 뉴스컨텐츠의 갯수: ' + hNews);
+    //.lead-more 함수호출!
+    more();
 
-        $(onNews).find('li:hidden').slice(0, 3).slideDown(500);
-
-        //숨겨진 뉴스가 없을때 버튼 없애기
-        if (hNews === 0) {
-            $('.load-more').fadeOut();
-            return false;
-        }
-
-        //숨겨진 뉴스 나오면서 스크롤 애니메이션
-        var offTop = $(this).offset().top;
-        console.log(offTop);
-
-        $('html, body').animate({
-            scrollTop: offTop - 300
-        }, 1000);
-
-    });
 
     ////////////////// 뉴스 카테고리 탭메뉴 //////////////////
     $('.tab-menu li a').click(function (evt) {
@@ -55,6 +51,8 @@ $(document).ready(function () {
             $('.load-more').hide();
         } else {
             $('.load-more').show();
+            $(onNews).siblings().hide();
+            $(onNews).find('li').slice(6, onNewsCount).hide();
         }
 
         //해당 탭메뉴 내용 등장
@@ -62,6 +60,45 @@ $(document).ready(function () {
 
         $(aHref).show().siblings().hide();
 
+        //.lead-more 함수호출!
+        more();
+
     });
 
 });
+
+
+//.lead-more 버튼효과
+function more() {
+
+    var onNews = $('.tab-menu .on').find('a').attr('href');
+    //console.log('기본 활성화된 뉴스: ' + onNews);
+
+    //활성화 된 뉴스 컨텐츠 li 갯수
+    var onNewsCount = $(onNews).find('li').length;
+    //console.log('li의 갯수: ' + onNewsCount);
+
+    $('.load-more').on('click', function () {
+
+        var hNews = $(onNews).find('li:hidden').length;
+        console.log('숨겨진 뉴스컨텐츠의 갯수: ' + hNews);
+
+        $(onNews).find('li:hidden').slice(0, 3).slideDown(500);
+
+        //숨겨진 뉴스가 없을때 버튼 없애기
+        if (hNews <= 3) {
+            $('.load-more').fadeOut();
+            return false;
+        }
+
+        //숨겨진 뉴스 나오면서 스크롤 애니메이션
+        var offTop = $(this).offset().top;
+        console.log(offTop);
+
+        $('html, body').animate({
+            scrollTop: offTop - 300
+        }, 1000);
+
+    });
+
+}
