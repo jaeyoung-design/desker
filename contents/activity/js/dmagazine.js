6b52e0ec6f6e53112f7fc9dd305d5f96fe146831
jaeyoung-design <sjy980130@naver.com>
2024-12-24 13:58:57 +0900

    dmagazine 수정

--- contents/activity/js/dmagazine.js
+++ contents/activity/js/dmagazine.js
@@ -2,22 +2,30 @@
 
 
 //alert('test!')
-$(document).ready(function(){
+$(document).ready(function () {
 
     ////////////////// load more 구현 //////////////////
     //초기설정
-    $('.news-wrap li').hide();
-    $('.news-wrap li').slice(0,6).show();
+    //활성화된 뉴스 컨텐츠 제외 모두 숨기기
+    var onNews = $('.tab-menu .on').find('a').attr('href');
+    //console.log('기본 활성화된 뉴스: ' + onNews);
 
-    $('.load-more').on('click', function(){
+    var onNewsCount = $(onNews).find('li').length;
+    console.log(onNewsCount);
 
-        var hNews = $('.news-wrap li:hidden').length;
+    $(onNews).siblings().hide();
+    //활성화된 뉴스 컨텐츠 중에서 보여질 구간 설정
+    $(onNews).find('li').slice(6, onNewsCount).hide();
+
+    $('.load-more').on('click', function () {
+
+        var hNews = $(onNews).find('li:hidden').length;
         console.log('숨겨진 뉴스컨텐츠의 갯수: ' + hNews);
 
-        $('.news-wrap li:hidden').slice(0,3).slideDown(500);
+        $(onNews).find('li:hidden').slice(0, 3).slideDown(500);
 
         //숨겨진 뉴스가 없을때 버튼 없애기
-        if (hNews === 0){
+        if (hNews === 0) {
             $('.load-more').fadeOut();
             return false;
         }
@@ -33,11 +41,25 @@ $(document).ready(function(){
     });
 
     ////////////////// 뉴스 카테고리 탭메뉴 //////////////////
-    $('.tab-menu li a').click(function(evt){
+    $('.tab-menu li a').click(function (evt) {
 
         evt.preventDefault();
 
         $(this).parent().addClass('on').siblings().removeClass('on');
+
+        var onNews = $('.tab-menu .on').find('a').attr('href');
+        var onNewsCount = $(onNews).find('li').length;
+
+        if (onNewsCount <= 6) {
+            $('.load-more').hide();
+        } else {
+            $('.load-more').show();
+        }
+
+        //해당 탭메뉴 내용 등장
+        var aHref = $(this).attr('href');
+
+        $(aHref).show().siblings().hide();
 
     });
 
